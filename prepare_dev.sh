6331015d029fdac344a751adf518e3170145f147
Baris Vural <bariscan.vural@bloomreach.com>
2018-09-08 13:04:59 +0200

    Update dev script

--- prepare_dev.sh
+++ prepare_dev.sh
@@ -4,6 +4,7 @@
 
 #Remove and recreate an empty hippo project
 rm -rf hippoproject/*
+mkdir -p hippoproject
 cd hippoproject
 echo -ne "\n" | mvn org.apache.maven.plugins:maven-archetype-plugin:2.4:generate \
 -DarchetypeRepository=https://maven.onehippo.com/maven2 \
